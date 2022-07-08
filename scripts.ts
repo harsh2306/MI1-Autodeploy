ebfbf376f8889013379c4e1a9ceea3a772c05f49
Rishabh <rishabhbhatia57@gmail.com>
2022-07-08 23:02:01 +0530

    changes to launch.html

--- scripts.ts
+++ scripts.ts
@@ -89,10 +89,11 @@ const urlParams = new URLSearchParams(queryString);
 // let PatientId= urlParams.get('Patientid')
 // let PatientId = document.getElementById("smartTextBox")
 let PatientId = document.getElementById("fhirpatientid").innerHTML
-let MI1_Client_ID= urlParams.get('MI1ClientID')
+// let MI1_Client_ID= urlParams.get('MI1ClientID')
+let MI1_Client_ID= document.getElementById("MI1ClientId").innerHTML
 // let MI1_Client_ID= window.MI1ClientId
 
-console.log(PatientId)
+console.log(PatientId, MI1_Client_ID)
 
 // generate id 
 // let MI1_Client_ID = 123456789
